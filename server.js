1eb7fad5cb851aaa0994a678111af9526895de30
Arshi Annafi <aannafi@uvic.ca>
2017-01-12 20:18:48 -0800

    added basic server

---
+++ server.js
@@ -0,0 +1,25 @@
+
+var http = require("http");
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var server = http.createServer(app);
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+/****************************** Express Routes ********************************/
+app.get("/", function (req, res) {
+    res.end("Hello world");
+});
+
+app.get("/data", function (req, res) {
+    res.end("{data: data}");
+});
+
+/******************************** Port assignment *****************************/
+server.listen(3000, function () {
+    console.log("Listening on port 3000");
+});
