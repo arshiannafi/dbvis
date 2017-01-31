5246dd384f6af95f95cb59db59366db69d90e802
bpattie <bpattie@uvic.ca>
2017-01-31 15:11:40 -0800

    added basic db-visualization view

--- public/js/view.js
+++ public/js/view.js
@@ -25,6 +25,12 @@ class ViewController {
                     view.jqref.hide();
                 }
             });
+
+            if (v.parentId !== undefined) {
+                //recursively show parent elements
+                this.show(v.parentId);
+            }
+
         } else {
             console.log("[ERROR] view " + id + " not found");
         }
@@ -53,27 +59,34 @@ which store callback functions that can be called from the show/hide functions
 of ViewController
 
 Groups:
-0 - Entire Window
+0 - Home (group 1 collectively), DB Schema visualization
 1 - Existing Projects, New Project, Edit Project
 
 */
 class View {
 
-    constructor(id, group) {
-        this.id     = id;
-        this.group  = group;
-        this.jqref  = $("#" + id);
+    constructor(id, group, parentId) {
+        this.id         = id;
+        this.parentId   = parentId
+        this.group      = group;
+        this.jqref      = $("#" + id);
     }
 
 }
 
 //TESTING
 vc = new ViewController();
-view = new View("view-select-proj", 1);
+
+view = new View("view-home", 0);
 vc.add(view);
-view = new View("view-create-proj", 1);
-vc.add(view);
-view = new View("view-edit-proj", 1);
+view = new View("view-db-vis", 0);
 vc.add(view);
 
-vc.show("view-create-proj");
+view = new View("view-select-proj", 1, "view-home");
+vc.add(view);
+view = new View("view-create-proj", 1, "view-home");
+vc.add(view);
+view = new View("view-edit-proj", 1, "view-home");
+vc.add(view);
+
+vc.show("view-db-vis");
